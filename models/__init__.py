84e2ae006b5d907aff6ce4d13448373dcb7c2037
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-17 15:24:23 +0000

    Add task 10

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 import os
+# Import all models
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE', 'fs')
 
--- models/place.py
+++ models/place.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """This module defines a class Place"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 # from models.review import Review
 import os
 import models
+
+# Define table for creating many-to-many relationship between Place and Amenity
+place_amenity = Table(
+    'place_amenity',
+    Base.metadata,
+    Column(
+        'place_id', String(60),
+        ForeignKey('places.id'),
+        primary_key=True,
+        nullable=False),
+    Column(
+        'amenity_id',
+        String(60),
+        ForeignKey('amenities.id'),
+        primary_key=True,
+        nullable=False)
+)
 
 
 class Place(BaseModel, Base):
@@ -25,9 +42,17 @@ class Place(BaseModel, Base):
 
     user = relationship('User', back_populates='places')
     cities = relationship('City', back_populates='places')
+
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship(
                 'Review', back_populates='place', cascade='all, delete-orphan')
+
+        amenities = relationship(
+            'Amenity',
+            secondary=place_amenity,
+            viewonly=False,
+            back_populates='place_amenities'
+        )
     else:
         @property
         def reviews(self):
@@ -41,3 +66,25 @@ class Place(BaseModel, Base):
                 if review_skk.place_id == self.id:
                     my_list.append(review_skk)
             return my_list
+
+        @property
+        def amenities(self):
+            """Getter for amenities when using FileStorage"""
+            from models.amenity import Amenity
+            return [
+                models.storage.get(Amenity, amenity_id)
+                for amenity_id in self.amenity_ids
+            ]
+
+        @amenities.setter
+        def amenities(self, amenity):
+            """Setter for amenities when using FileStorage"""
+            from models.amenity import Amenity
+            # amenity_ids = []
+            if isinstance(amenity, Amenity):
+                if amenity.id not in self.amenity_ids:
+                    self.amenity_ids.append(amenity.id)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.amenity_ids = []
