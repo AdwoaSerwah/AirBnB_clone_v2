3810dd1036f87e65afbd6ed1d1c55e77f1bac539
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-20 16:28:58 -0700

    Try again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Fabric script to generate a .tgz archive from the contents
-of the web_static folder.
+of the web_static folder and deploy it to the web servers.
 """
 
 from fabric.api import local, run, env, put
@@ -11,29 +11,46 @@ import os
 env.hosts = ['100.25.177.89', '100.25.188.96']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
+executed_locally = False
 
 
 def do_deploy(archive_path):
     """
     Distributes an archive to web servers.
     """
+    global executed_locally
+
     if not os.path.exists(archive_path):
         print("Path does not exist!")
         return False
 
     try:
-        result = put(archive_path, '/tmp/')
-        # print("Put result:", result.succeeded)
         arch_name = os.path.basename(archive_path)
-        new_name = arch_name.split(".")[0]
+        new = arch_name.split(".")[0]
         the_path = "/data/web_static/releases/"
-        run('mkdir -p {}{}/'.format(the_path, new_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(arch_name, the_path, new_name))
+
+        # Only execute this block once locally
+        if not executed_locally:
+            local(f'cp {archive_path} /tmp/')
+            local('mkdir -p {}{}/'.format(the_path, new))
+            local('tar -xzf /tmp/{} -C {}{}/'.format(arch_name, the_path, new))
+            local('rm /tmp/{}'.format(arch_name))
+            local('mv {0}{1}/web_static/* {0}{1}/'.format(the_path, new))
+            local('rm -rf {}{}/web_static'.format(the_path, new))
+            local('rm -rf /data/web_static/current')
+            local('ln -s {}{}/ /data/web_static/current'.format(the_path, new))
+            executed_locally = True
+
+        # Remote operations
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(the_path, new))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(arch_name, the_path, new))
         run('rm /tmp/{}'.format(arch_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(the_path, new_name))
-        run('rm -rf {}{}/web_static'.format(the_path, new_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(the_path, new))
+        run('rm -rf {}{}/web_static'.format(the_path, new))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(the_path, new_name))
+        run('ln -s {}{}/ /data/web_static/current'.format(the_path, new))
+
         return True
     except Exception as e:
         print("An error occurred:", e)
