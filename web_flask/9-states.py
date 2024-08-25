fadd257142ea1922570583662b4bef618ebfd724
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-25 09:41:45 +0000

    Add task 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Flask web application to display states and cities."""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from os import getenv
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """Closes the storage on teardown."""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
+    """Display a HTML page with states or cities depending on the route."""
+    states = storage.all(State)
+    if id:
+        state = states.get('State.' + id)
+        if state:
+            return render_template('9-states.html', state=state)
+        else:
+            return render_template('9-states.html')
+    else:
+        return render_template('9-states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
