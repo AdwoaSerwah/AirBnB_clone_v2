e75c9cbc4b3965d8232e545b0b956cb8f494cf46
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-25 07:49:17 +0000

    Add

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""Flask web application that displays a list of states"""
-
-from flask import Flask, render_template
+""" Starts a Flash Web Application """
 from models import storage
 from models.state import State
-
+from flask import Flask, render_template
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
 def close_db(error):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays a list of all State objects sorted by name"""
+    """ displays a HTML page with a list of states """
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
